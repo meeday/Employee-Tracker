c2ca55544379f066b50c82010afd70d9e59558a4
meeday <59993824+meeday@users.noreply.github.com>
2020-06-21 19:27:41 +0100

    connection converted into class

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,12 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Nightrise-1!",
-  database: "employee_db"
-});
-
-connection.connect(err => {
-  if(err) {
-    console.error("error connecting: " + err.stack);
-    return;
+class connection {
+  constructor(host,port,user,password,database){
+    this.host = host;
+    this.port = port;
+    this.user = user;
+    this.password = password;
+    this.database = database;
   }
-  console.log("connected as id " + connection.threadId);
-});
-
+}
 module.exports = connection;
