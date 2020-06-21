e4f3edb3dd76a9c3ebe140ce1f2c378d8d1c23f7
meeday <59993824+meeday@users.noreply.github.com>
2020-06-21 19:35:27 +0100

    files and modules required into index file

---
+++ index.js
@@ -0,0 +1,6 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+const UserSearch = require("./lib/UserSearch");
+const Questions = require("./lib/questions");
