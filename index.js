2a15190f1dfd2c539e4024a86b59138e250219e3
meeday <59993824+meeday@users.noreply.github.com>
2020-06-21 19:38:15 +0100

    code for main menu added

--- index.js
+++ index.js
@@ -12,3 +12,39 @@ const init = () => {
   console.log("Welcome to Employee Tracker");
   loadMenu();
 }
+
+const loadMenu = async () => {
+  await inquirer.prompt(questions.startPage)
+  .then(async answers => {
+      switch(answers.choice){
+          case "View All Employees":
+              await getAllEmployeesFullData();                
+              break;
+          case "View Departments":
+              await viewAllDepartments();                
+              break;
+          case "View Roles":
+              await viewAllRoles();                
+              break;
+          case "View employees by Manager":
+              await viewEmployeesByManager();
+              break;
+          case "Add a Department":
+              await addDepartment();
+              break;
+          case "Add a Role":
+              await addRole();
+              break;
+          case "Update Employee Role":
+              await updateEmployeeRole();
+              break;
+          case "Add an Employee":
+              await addEmployee();
+              break;
+          case "End":     
+              break;
+          default:
+              break;
+      }        
+  });   
+}
